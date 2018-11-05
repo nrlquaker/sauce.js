a0e6361f120ae27c5a1665d5099c4c294f6ad455
nrlquaker <ustynov.vy@gmail.com>
2018-11-05 18:35:17 +0200

    Move magic numbers to constants

--- lib/sauce/sauce-parser.ts
+++ lib/sauce/sauce-parser.ts
@@ -4,8 +4,11 @@ import getDataTypeForCode from './data-types'
 import Sauce from './sauce'
 
 export default class SauceParser {
-    private readonly sauceSize = 128
-    private readonly commentSize = 64
+    private static readonly SAUCE_ID = 'SAUCE'
+    private static readonly COMMENT_ID = 'COMNT'
+    private static readonly SAUCE_SIZE = 128
+    private static readonly COMMENT_SIZE = 64
+    private static readonly COMMENT_ID_SIZE = 5
 
     public parse(filePath: string): Sauce | null {
         this.validateFile(filePath)
@@ -14,9 +17,9 @@ export default class SauceParser {
         const buffer = Buffer.alloc(fullFileSize)
         fs.readSync(fd, buffer, 0, fullFileSize, 0)
         const reader = new BufferReader(buffer)
-        reader.seek(fullFileSize - this.sauceSize)
+        reader.seek(fullFileSize - SauceParser.SAUCE_SIZE)
         const id = reader.nextString(5)
-        if (id !== 'SAUCE') {
+        if (id !== SauceParser.SAUCE_ID) {
             return null
         }
         const version = reader.nextString(2)
@@ -57,15 +60,21 @@ export default class SauceParser {
 
     private parseComments(reader: any, fullFileSize: number,
                           commentLines: number): Array<string> {
-        reader.seek(fullFileSize - this.sauceSize - commentLines * this.commentSize - 5)
-        const commentId = reader.nextString(5)
+        reader.seek(this.commentPosition(fullFileSize, commentLines))
+        const commentId = reader.nextString(SauceParser.COMMENT_ID_SIZE)
         const result: Array<string> = []
-        if (commentId === 'COMNT') {
+        if (commentId === SauceParser.COMMENT_ID) {
             for (let i = 0; i < commentLines; i++) {
-                const line = reader.nextString(this.commentSize).trim()
+                const line = reader.nextString(SauceParser.COMMENT_SIZE).trim()
                 result.push(line)
             }
         }
         return result
+    }
+
+    private commentPosition(fullFileSize: number, commentLines: number): number {
+        const commentsSize =
+            SauceParser.COMMENT_ID_SIZE + commentLines * SauceParser.COMMENT_SIZE
+        return fullFileSize - SauceParser.SAUCE_SIZE - commentsSize
     }
 }
